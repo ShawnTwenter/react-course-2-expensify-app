6c790bfc88477d77524509927bb3aa8c87a432e9
Shawn Twenter <shawn.twenter@storage-mart.com>
2018-04-03 12:57:04 -0500

    Setup production webpack build

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Git Commands
+
+git init - Create a new git repo
+git status - view the changes to your project
+git add - add files to staging area
+git commit - creates a commit with files form staging
+git log -
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+module.exports = (env, argv) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    
+ return  {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -16,18 +21,33 @@ module.exports = {
             },
             {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader:'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
             }
         ]
     },
-    devtool: 'cheap-module-eval-source-map',
+    plugins: [
+        CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true 
     }
 };
+}
 
